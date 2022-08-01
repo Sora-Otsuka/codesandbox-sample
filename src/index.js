14520149e94b548bb6f9b2c2db2e33263c6c2e26
Sora-Otsuka <110004466+Sora-Otsuka@users.noreply.github.com>
2022-08-01 14:02:13 +0900

    innertextの修正

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 const onClickAdd = () => {
   // テキストボックスの値を取得し初期化
-  const inputText = document.getElementById('add-text').value;
-  document.getElementById('add-text').value = '';
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
   // liタグを生成
   const li = document.createElement("li");
 
   // divタグを生成
-  const div = document.createElement('div');
-  div.className = 'list-row';
+  const div = document.createElement("div");
+  div.className = "list-row";
 
   // pタグを生成
-  const p = document.createElement('p');
+  const p = document.createElement("p");
   p.innerText = inputText;
 
   // button(完了)
-  const completeButton = document.createElement('button');
-  completeButton.innerText('完了');
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
 
   // button(削除)
-  const deleteButton = document.createElement('button');
-  deleteButton.innerText('削除');
-
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
 
   // ulの子要素にli
-  document.getElementById('incomplete-list').appendChild(li);
+  document.getElementById("incomplete-list").appendChild(li);
 
   // 子要素の配置
   // liの子要素にdiv
@@ -35,7 +34,6 @@ const onClickAdd = () => {
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
   // どうなるか？
-
 };
 
 document
