347b2dec4fc42c11f8b3fb67da9fc80818948ba6
Sora-Otsuka <aitech.colabo.otsuka@gmail.com>
2022-08-01 13:56:02 +0900

    ボタンの追加

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const onClickAdd = () => {
 
   // liタグを生成
   const li = document.createElement("li");
-  
+
   // divタグを生成
   const div = document.createElement('div');
   div.className = 'list-row';
@@ -14,14 +14,28 @@ const onClickAdd = () => {
   const p = document.createElement('p');
   p.innerText = inputText;
 
+  // button(完了)
+  const completeButton = document.createElement('button');
+  completeButton.innerText('完了');
+
+  // button(削除)
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText('削除');
+
+
+  // ulの子要素にli
+  document.getElementById('incomplete-list').appendChild(li);
+
+  // 子要素の配置
   // liの子要素にdiv
   li.appendChild(div);
 
   // divの子要素にp
-  div.appendChild(p)
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
-  // ulの子要素にli
-  document.getElementById('incomplete-list').appendChild(li)
+
 };
 
 document
